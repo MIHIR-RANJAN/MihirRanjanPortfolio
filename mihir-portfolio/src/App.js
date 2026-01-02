6bf3a5d0f8ccd31e6ff656fb33e3ecc4a408527a
MIHIR RANJAN <mihir29062001@gmail.com>
2026-01-02 18:20:50 +0530

    added new resume

--- mihir-portfolio/src/App.js
+++ mihir-portfolio/src/App.js
@@ -345,7 +345,7 @@ export default function Home() {
             {/* RESUME BOX BUTTON */}
             <li>
               <a
-                href="https://drive.google.com/file/d/1S6xrstpAGuSFuEnCh-5PnVLdJft5pm8n/view?usp=sharing"
+                href="https://drive.google.com/file/d/1lTsqKuBAQhTG4J3ou5jmrr8c0aMDt_Q1/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-md text-sm transition-all duration-300"
@@ -399,7 +399,7 @@ export default function Home() {
           isVisible ? 'opacity-100 transform translate-y-0' : 'opacity-0 transform translate-y-8'
         }`}>
           <a 
-            href="https://drive.google.com/file/d/1S6xrstpAGuSFuEnCh-5PnVLdJft5pm8n/view?usp=sharing" 
+            href="https://drive.google.com/file/d/1lTsqKuBAQhTG4J3ou5jmrr8c0aMDt_Q1/view?usp=sharing" 
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-xl text-white font-semibold shadow-md transition-colors duration-300"
@@ -977,7 +977,7 @@ export default function Home() {
 
                 {/* Resume Button */}
                 <a
-                  href="https://drive.google.com/file/d/1S6xrstpAGuSFuEnCh-5PnVLdJft5pm8n/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1lTsqKuBAQhTG4J3ou5jmrr8c0aMDt_Q1/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex items-center gap-2 border border-gray-500 px-5 py-2 rounded-full text-white hover:bg-white hover:text-black transition"
@@ -993,7 +993,7 @@ export default function Home() {
      
       <div className="bg-white text-black w-full py-4 px-6 z-20 relative flex flex-col md:flex-row justify-between items-center">
         {/* Left Text */}
-        <p className="text-sm font-medium">&copy; 2025 Mihir Ranjan. All rights reserved.</p>
+        <p className="text-sm font-medium">&copy; 2026 Mihir Ranjan. All rights reserved.</p>
 
         {/* Right Icons */}
         <div className="flex gap-4 mt-2 md:mt-0">
@@ -1019,3 +1019,4 @@ export default function Home() {
     </div>
   );
 }
+
